2f3b501e4a81296b19eacaf0481638219e5f8763
Aviatore <w.wesolowski@vp.pl>
2018-11-17 10:47:19 +0100

    Added double buffer variables and bufer-writing function

--- buffer_test.pl
+++ buffer_test.pl
@@ -6,6 +6,7 @@ use Storable qw(dclone);
 
 $| = 1;
 my @buffer = ();
+my @buffer_2 = ();
 my $scr = new Term::Screen;
 $scr->clrscr();
 my $x = $scr->cols();
@@ -15,12 +16,85 @@ for ( my $xx = 0; $xx < $x; $xx++ )
 {
   for ( my $yy = 0; $yy < $y; $yy++ )
   {
+    if ( ( $xx == 0 or $xx == ($x - 1) ) and $yy > 0 )
+    {
+      $buffer[$yy][$xx] = "|";
+    }
+    elsif ( $yy == 0 or $yy == ($y - 1 ) )
+    {
+      $buffer[$yy][$xx] = "_";
+    }
+    else
+    {
+      $buffer[$yy][$xx] = " ";
+    }
+    $buffer_2[$yy][$xx] = " ";
+
     my $coord = $xx . ":" . $yy;
     push @used, $coord;
   }
 }
 
-intro(\$scr,\@used,$x,$y);
+my @line = ();
+for ( my $i = 1; $i < ($y - 2); $i++ )
+{
+  push @line, [ ($i, $x - 2) ];
+}
+ write_buffer(\$scr, \@buffer, \@buffer_2, $x, $y);
+ exit;
+
+
+while(1)
+{
+  my $index = 0;
+
+  foreach my $pixel ( @line )
+  {
+    $buffer[ $line[$index]->[0] ][ $line[$index]->[1] ] = " ";
+    $index++;
+  }
+
+  foreach my $pixel ( @line )
+  {
+    $line[$index]->[1]--;
+    $index++;
+  }
+
+  foreach my $pixel ( @line )
+  {
+    $buffer[ $line[$index]->[0] ][ $line[$index]->[1] ] = "#";
+    $index++;
+  }
+
+  write_buffer(\$scr, \@buffer, \@buffer_2, $x, $y);
+
+  sleep 1;
+}
+
+sub write_buffer
+{
+  my ( $scr, $buffer, $buffer_2, $x, $y ) = @_;
+
+  for ( my $xx = 0; $xx < $x; $xx++ )
+  {
+    for ( my $yy = 0; $yy < $y; $yy++ )
+    {
+      if ( $$buffer[$yy][$xx] eq $$buffer_2[$yy][$xx] )
+      {
+        next;
+      }
+      else
+      {
+        $$scr->at( $yy, $xx );
+        print $buffer[$yy][$xx];
+
+        $$buffer_2[$yy][$xx] = $$buffer[$yy][$xx]; 
+      }
+    }
+  }
+}
+
+#intro(\$scr,\@used,$x,$y);
 
 sub intro
 {
